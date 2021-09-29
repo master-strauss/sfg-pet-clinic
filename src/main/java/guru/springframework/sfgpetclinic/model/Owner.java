4df3cd1386b4970aa41276fb4b9dfbdde4e824a2
MasterStrauss <blueplanet19x@gmail.com>
2021-09-29 13:38:38 +0100

    Adding POJO's.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgpetclinic.model;/* cucul created on 29/09/2021 inside the package - guru.springframework.sfgpetclinic.model */
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.model;/* cucul created on 29/09/2021 inside the package - guru.springframework.sfgpetclinic.model */
+
+public class PetType {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
