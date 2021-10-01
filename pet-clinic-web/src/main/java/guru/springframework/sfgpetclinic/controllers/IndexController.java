011f49231e6ecda7f75c98bb617bab1a8e722387
MasterStrauss <blueplanet19x@gmail.com>
2021-10-01 23:36:29 +0100

    Add Controller. Closes #8

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+/* cucul created on 01/10/2021 inside the package 
+- guru.springframework.sfgpetclinic.controllers */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
