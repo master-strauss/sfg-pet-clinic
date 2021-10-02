c689f9c998677a3009b1e358cf358f2652d550a7
MasterStrauss <blueplanet19x@gmail.com>
2021-10-02 15:48:43 +0100

    Adding  bootstrap for loading data.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.services;/* cucul created on 02/10/2021 inside the package - guru.springframework.sfgpetclinic.services */
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerMapService.java
@@ -4,11 +4,11 @@ package guru.springframework.sfgpetclinic.services.map;
 - guru.springframework.sfgpetclinic.services.map */
 
 import guru.springframework.sfgpetclinic.model.Owner;
-import guru.springframework.sfgpetclinic.services.CrudService;
+import guru.springframework.sfgpetclinic.services.OwnerService;
 
 import java.util.Set;
 
-public class OwnerMapService extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long> {
+public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
     
     @Override
     public Set<Owner> findAll() {
@@ -33,5 +33,13 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
     @Override
     public Owner findById(Long id) {
         return super.findById(id);
+    }
+    
+    public Owner findByLastName(String lastName) {
+        return this.findAll()
+                .stream()
+                .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
+                .findFirst()
+                .orElse(null);
     }
 }
